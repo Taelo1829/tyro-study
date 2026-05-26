6bcad02cbf561ade975045bba3a81e7548cfe250
Taelo <taelo@izon.group>
2026-05-26 14:53:02 +0200

    starting on main file

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,7 @@ const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 })
 
 export default withPWA(nextConfig);
